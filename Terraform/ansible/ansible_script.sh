20227e9738496f42b867fbecadd4ead9d7712564
razvan.caplea <caplearazvan2@gmail.com>
2024-02-05 17:41:02 +0200

    update stable variant of project

--- Terraform/ansible/ansible_script.sh
+++ Terraform/ansible/ansible_script.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash 
+sleep 300
 
-sudo apt-get update && sudo apt-get install -y ansible 
+sudo apt-get upgrade -y && sudo apt update && sudo apt install -y ansible
 
-echo "running playbook"
-sudo ansible-playbook -u ubuntu /home/ubuntu/playbook.yaml
+ansible-playbook /home/ubuntu/playbook.yaml
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,58 +1,74 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
   region = "eu-central-1"
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-resource "aws_instance" "app_server" {
-  ami                    = "ami-0faab6bdbac9486fb"
-  instance_type          = "t2.micro"
-  key_name               = "amazon-key"
-  vpc_security_group_ids = ["sg-07be6af9e65dc42e2", "sg-080b3cf708d8bdb28", "sg-0346a684705805e2d"]
-  subnet_id              = "subnet-048b7ac9cf0935aca"
-  #user_data              = file("script.sh") 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
 
-  tags = {
-    Name = "App Server"
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
-  connection {
-    type = "ssh"
-    user        = "ubuntu"
-    private_key = file("./amazon-key.pem")
-    host = self.public_ip
-  }
-   provisioner "file" {
-    source      = "./ansible/playbook.yaml"
-    destination = "/home/ubuntu/playbook.yaml"
-  }
-  provisioner "file" {
-    source      = "./ansible/ansible_script.sh"
-    destination = "/home/ubuntu/ansible_script.sh"
-  }
-  # execute on the EC2-instance .sh file
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /home/ubuntu/ansible_script.sh",
-      "/home/ubuntu/ansible_script.sh",
-    ]
-  }
+
+  owners = ["099720109477"] # Canonical
 }
+
+resource "aws_instance" "web" {
+    ami           = data.aws_ami.ubuntu.id
+    instance_type = "t3.micro"
+    key_name               = "amazon-key"
+    vpc_security_group_ids = ["sg-07be6af9e65dc42e2", "sg-080b3cf708d8bdb28", "sg-0346a684705805e2d"]
+    subnet_id              = "subnet-048b7ac9cf0935aca"
+
+    tags = {
+        Name = "App Server"
+    }
+
+    connection {
+        type     = "ssh"
+        user     = "ubuntu"
+        private_key = file("amazon-key.pem")
+        host     = self.public_ip
+    }
+
+    provisioner "file" {
+        source = "ansible/ansible_script.sh"
+        destination = "/home/ubuntu/ansible_script.sh"
+    }
+
+    provisioner "file" {
+        source = "ansible/playbook.yaml"
+        destination = "/home/ubuntu/playbook.yaml"
+    }
+
+    provisioner "remote-exec" {  
+    inline = [  
+    "echo 'Waiting for user data script to finish'",  
+    "cloud-init status --wait > /dev/null"  
+    ]  
+}   
+    # provisioner "local-exec" {
+    #     command = "sleep 300"
+    # }
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /home/ubuntu/ansible_script.sh",
+            "/home/ubuntu/ansible_script.sh"
+        ]
+    }
+}
+
 # resource "aws_instance" "jenkins_server" {
 #   ami                    = "ami-0faab6bdbac9486fb"
 #   instance_type          = "t2.micro"
 #   key_name               = "amazon-key"
 #   vpc_security_group_ids = ["sg-07be6af9e65dc42e2", "sg-080b3cf708d8bdb28", "sg-0346a684705805e2d"]
 #   subnet_id              = "subnet-048b7ac9cf0935aca"
-#   user_data              = file("script.sh") 
+# #   user_data              = file("script.sh") 
 
 #   tags = {
 #     Name = "Jenkins Server"
